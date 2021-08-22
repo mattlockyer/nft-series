f153452ef9bf017cda5d043c22c738141a36011e
mattlockyer <mattdlockyer@gmail.com>
2021-08-22 08:09:54 -0700

    remove commented code unused

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -50,9 +50,6 @@ pub struct Contract {
 	// CUSTOM
 	token_type_by_title: LookupMap<TokenTypeTitle, TokenTypeId>,
 	token_type_by_id: UnorderedMap<TokenTypeId, TokenType>,
-	// token_types: UnorderedMap<TokenTypeId, TokenMetadata>,
-	// type_owners: LookupMap<TokenTypeId, AccountId>,
-	// tokens_by_type: LookupMap<TokenTypeId, UnorderedSet<TokenId>>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -68,8 +65,6 @@ enum StorageKey {
     TokenTypeByTitle,
     TokenTypeById,
     TokensByTypeInner { token_type_id: u64 },
-    // TypeOwners,
-	// TokensByType,
 }
 
 #[near_bindgen]
@@ -104,8 +99,6 @@ impl Contract {
             ),
 			token_type_by_id: UnorderedMap::new(StorageKey::TokenTypeById),
 			token_type_by_title: LookupMap::new(StorageKey::TokenTypeByTitle),
-			// type_owners: LookupMap::new(StorageKey::TypeOwners),
-			// tokens_by_type: LookupMap::new(StorageKey::TokensByType),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
         }
     }
